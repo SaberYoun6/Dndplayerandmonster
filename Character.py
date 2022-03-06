d0239ac10e886501b922b216bec1816ba1d431b0
yuriprym <samuel.young.103@gmail.com>
2022-03-06 11:11:16 -0500

    add changes to PCs and character class

--- Character.py
+++ Character.py
@@ -12,49 +12,45 @@ __status__ = "Aplha"
 ### depedencies ####
 
 
-pmat= 6 + 4
-
-def setting_up_character():
-    charater_Name = input("Please put your character name")
-   character_attribute = []
-   for status in character_attributes:
-       try:
-        all_status= input("level, hitpoints, armor_class, initivate,str,dex,con,int,wid,cha ")
-        a_status=all_status.split(", ")
-        level = int(a_status[0])
-        hitpoints=int(a_status[1])
-        armour_class  = int(a_status[2])
-        initivative = int(a_status[3])
-        strength = int(a_status[4])
-        dexitery = int(a_status[5])
-        constution = int(a_status[6])
-        intelligence = int(a_status[7])
-        wisdom = int(a_status[8])
-        charistma = int(a_status[9])
-        break:
-    except:
-        print("Oops!", sys.exc_info()[0],"occurred.")
-        print("Next entry.")
-        print()
-    character_attribute.append(level)
-    character_attribute.append(hitpoint)
-    character_attribute.append(armour_class)
-    character_attribute.append(initivative)
-    character_attribute.append(strength)
-    character_attribute.append(dexitery)
-    character_attribute.append(constution)
-    character_attribute.append(intelligence)
-    character_attribute.append(wisdom)
-    character_attribute.append(charistma)
-    return dict(charater_Name,character_attribute)
-
+class Characters():
+    def setting_up_character():
+        charater_Name = input("Please put your character name")
+        character_attribute = []
+"""character_Attributes = [hit_points, armor_class, initivate
+, str, dex, con,
+int, wid, cha]""" 
+        for status in character_attributes:
+            try:
+                all_status= input("hitpoints, armor_class, initivate,str,dex,con,int,wid,cha ")
+                a_status=all_status.split(", ")
+                hitpoints=int(a_status[0])
+                armour_class  = int(a_status[1])
+                initivative = int(a_status[2])
+                strength = int(a_status[3])
+                dexitery = int(a_status[4])
+                constution = int(a_status[5])
+                intelligence = int(a_status[6])
+                wisdom = int(a_status[7])
+                charistma = int(a_status[8])
+                break:
+            except:
+                print("Oops!", sys.exc_info()[0],"occurred.")
+                print("Next entry.")
+                print()
+        character_attribute.append(hitpoint)
+        character_attribute.append(armour_class)
+        character_attribute.append(initivative)
+        character_attribute.append(strength)
+        character_attribute.append(dexitery)
+        character_attribute.append(constution)
+        character_attribute.append(intelligence)
+        character_attribute.append(wisdom)
+        character_attribute.append(charistma)
+        return dict(charater_Name,character_attribute)
 
 
     
-"""character_Attributes = [hit_points, armor_class, initivate, str, dex, con, int, wid, cha] 
 """
-character_Attributes = 
-fullcharacters= dict(charater_Name,character_attributes)
 
 
 
---
+++ PlayerCharacter.py
@@ -0,0 +1,20 @@
+import Character
+class player__character__():
+    def __init__(self, name,level, player_character_class):
+        self.name = name
+        self.level =level
+        self.player_character_class=player_character_class
+    def player_characters_setup():
+
+    def player_character_class(self)
+      if self.player_character_class == "Bard":
+          num = 1
+      elif self.player_character_class == "Barbarian":
+          num = 2
+      elif self.player_character_class == "Cleric":
+          num = 3
+      elif self.player_character_class == "artificer":
+          num = 4
+      elif self.player_character_class == "monk":
+          num = 5 
+
